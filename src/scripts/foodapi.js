8fb647a6f02bd508c33a2bcf4eb27df22edce62a
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-03-27 10:18:57 -0500

    Working on this again

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,8 +1,15 @@
 console.log("ohai")
 
+const foodFactory = (food => {
+    
+})
+
 fetch("http://localhost:8088/foods")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods)
+        parsedFoods.forEach(food => {
+            const foodAsHTML = foodFactory(food)
+            addFoodToDom(foodAsHTML)
+        })
     })
 
