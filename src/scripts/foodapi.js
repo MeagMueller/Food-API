0c22a6bd54189b198262745f3ecdad06bbfb77d7
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-03-28 15:08:35 -0500

    Added stuff

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -2,11 +2,13 @@ console.log("ohai")
 
 const foodDisplay = document.querySelector("#foodList")
 
+
 const nomsOnDom = (domNoms) => {
+    
     return `<div class="eachNom">
                 <h2>${domNoms.name}</h2>
                 <p>${domNoms.category}</p>
-                <p>${domNoms.ethnicity} 
+                <p>${domNoms.ethnicity}</p>
             </div>`
 }
 
@@ -17,10 +19,18 @@ fetch("http://localhost:8088/foods")
             fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
             .then(response => response.json())
             .then(productInfo => {
-                food.
+                parsedFoods.forEach(product => {
+                    food.ingredients = productInfo.product.ingredients
+                    // food.country = productInfo.product.countries_hierarchy
+                    // food.calories = productInfo.product.nutriments.energy
+                    // food.sugar = productInfo.product.nutriments.sugars
+                    // food.fat = productInfo.product.nutriments.fat
+                    
+                })
+
                 foodDisplay.innerHTML += nomsOnDom(food)
+
             })
         })
 })
-
 
